1e77ea45c8b6f532b879e7deacf6d5498a6a1936
toan lam <lamtoan557@gmail.com>
2022-03-09 13:10:17 +0700

    Search

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,6 +1,30 @@
 class DevelopersController < ApplicationController
-
+    
     def index
         @developers = Developer.all
     end
+
+    def new
+        
+    end
+
+    def get
+        search_dev_name = ProgrammingLanguage.find_by(name: params[:name])
+        search_dev_code = Language.find_by(code: params[:code])
+        arr = []
+        if search_dev_name && search_dev_code
+            arr = search_dev_name.developers.to_a.intersection(search_dev_code.developers.to_a)
+            @@dev = arr
+            redirect_to "/result" 
+        else
+            redirect_to "/"
+        end
+        # rescue StandardError => e
+        #     respond_with_error(e)
+    end
+
+    def displayresult
+        @dev = @@dev
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  get "result", to: "developers#displayresult"
+
+
+  post "search", to: "developers#get"
+  get "search", to: "developers#new"
+
   get "/", to: "developers#index"
 end

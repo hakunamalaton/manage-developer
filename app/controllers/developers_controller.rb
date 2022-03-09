43b5300d8a168bcc7fa729564864db375bd25f68
toan lam <lamtoan557@gmail.com>
2022-03-09 16:55:32 +0700

    fix search many languages

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -9,41 +9,53 @@ class DevelopersController < ApplicationController
     end
 
     def get
-        search_dev_name = nil
+        search_dev_name = []
+        search_dev_code = []
         @@dev = []
         if params[:name].length > 0
-          search_dev_name = ProgrammingLanguage.find_by(name: params[:name]) 
-          if search_dev_name == nil
-            redirect_to "/result" and return
-          end
-          search_dev_name = search_dev_name.developers.to_a
-        end
+          programming_lan = params[:name].split(",").map(&:strip).map(&:downcase).map(&:capitalize)
 
-        search_dev_code = nil
+          programming_lan.each do |lan|
+            if ProgrammingLanguage.find_by(name: lan) 
+              if search_dev_name.empty?
+                search_dev_name = ProgrammingLanguage.find_by(name: lan).developers.to_a
+              else
+                search_dev_name = search_dev_name.intersection(ProgrammingLanguage.find_by(name: lan).developers.to_a)
+              end
+            else
+              # no result
+              @@dev = []
+              redirect_to result_path and return
+            end
+          end
+
+          @@dev = search_dev_name
+        end
+        
         if params[:code].length > 0
-          search_dev_code = Language.find_by(code: params[:code]) 
-          if search_dev_code == nil
-            redirect_to "/result" and return
+          languages = params[:code].split(",").map(&:strip).map(&:downcase)
+
+          languages.each do |lan|
+            if Language.find_by(code: lan) 
+              if search_dev_code.empty?
+                search_dev_code = Language.find_by(code: lan).developers.to_a
+              else
+                search_dev_code = search_dev_code.intersection(Language.find_by(code: lan).developers.to_a)
+              end
+            else
+              # no result
+              @@dev = []
+              redirect_to result_path and return
+            end
           end
-          search_dev_code = search_dev_code.developers.to_a
-        end
 
-
-        if search_dev_name && search_dev_code
-            arr = search_dev_name.intersection(search_dev_code)
-            @@dev = arr
-           
-        elsif search_dev_name
-            arr = search_dev_name
-            @@dev = arr
-            
-        elsif search_dev_code
-            arr = search_dev_code
-            @@dev = arr
+          if params[:name].length > 0
+            @@dev = @@dev.intersection(search_dev_code) 
+          else
+            @@dev = search_dev_code
+          end
         end
-        redirect_to "/result"
-        # rescue StandardError => e
-        #     respond_with_error(e)
+        redirect_to result_path
     end
 
     def displayresult

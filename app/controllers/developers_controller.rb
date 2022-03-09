308f6b0ad8b16c19791e37f016e0eadbdd86c85c
toan lam <lamtoan557@gmail.com>
2022-03-09 13:31:03 +0700

    fix search

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -9,16 +9,39 @@ class DevelopersController < ApplicationController
     end
 
     def get
-        search_dev_name = ProgrammingLanguage.find_by(name: params[:name])
-        search_dev_code = Language.find_by(code: params[:code])
-        arr = []
-        if search_dev_name && search_dev_code
-            arr = search_dev_name.developers.to_a.intersection(search_dev_code.developers.to_a)
-            @@dev = arr
-            redirect_to "/result" 
-        else
-            redirect_to "/"
+        search_dev_name = nil
+        @@dev = []
+        if params[:name].length > 0
+          search_dev_name = ProgrammingLanguage.find_by(name: params[:name]) 
+          if search_dev_name == nil
+            redirect_to "/result" and return
+          end
+          search_dev_name = search_dev_name.developers.to_a
         end
+
+        search_dev_code = nil
+        if params[:code].length > 0
+          search_dev_code = Language.find_by(code: params[:code]) 
+          if search_dev_code == nil
+            redirect_to "/result" and return
+          end
+          search_dev_code = search_dev_code.developers.to_a
+        end
+
+
+        if search_dev_name && search_dev_code
+            arr = search_dev_name.intersection(search_dev_code)
+            @@dev = arr
+           
+        elsif search_dev_name
+            arr = search_dev_name
+            @@dev = arr
+            
+        elsif search_dev_code
+            arr = search_dev_code
+            @@dev = arr
+        end
+        redirect_to "/result"
         # rescue StandardError => e
         #     respond_with_error(e)
     end
